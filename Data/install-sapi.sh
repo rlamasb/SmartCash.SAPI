ed927c66aa03dd3feb5de484b8626df603b5e389
Gonçalo Neves <stirred@protonmail.com>
2018-04-05 14:41:58 +0100

    fix(data/install-sapi.sh): nginx proxy header

--- Data/install-sapi.sh
+++ Data/install-sapi.sh
@@ -269,7 +269,7 @@ server {
     proxy_http_version 1.1;
     proxy_pass http://localhost:5000;
     proxy_pass_header Server;
-    proxy_set_header Connection 'upgrade';
+    proxy_set_header Connection keep-alive';
     proxy_set_header Host \$http_host;
     proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
